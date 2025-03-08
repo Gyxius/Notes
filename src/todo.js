85c11857f43283f4ca76bc8b57ecb16002146fe5
Gyxius <mitsou.fortunat@gmail.com>
2025-03-08 18:04:39 +0100

    add Tasks and Todo Classes

--- src/todo.js
+++ src/todo.js
@@ -1,40 +1,29 @@
 // The goal is to have a simple todolist
 
 class Tasks {
-    static idCounter = 0;
-    constructor(name) {
-        this.name = name;
-        this.id = Tasks.idCounter++; 
-    }
-    checkTask() {
-
-    }
-    editTask() {
-
-    }
+  static idCounter = 0;
+  constructor(name) {
+    this.name = name;
+    this.id = Tasks.idCounter++;
+    this.completed = false;
+  }
+  editTask(newName) {
+    this.name = newName;
+  }
 }
 
 class TODO {
-    constructor() {
-        this.tasks = [];
-    }
-    removeTask() {
-
-    }
-    addTask() {
-
-    }
-    editTask(){
-
-    }
-    checkTask() {
-
-    }
+  constructor() {
+    this.tasks = [];
+  }
+  removeTask(taskId) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+  addTask(task) {
+    this.tasks.push(task);
+  }
+  editTask(taskId, newName) {
+    currentTask = this.tasks.filter((task) => task.id === taskId);
+    currentTask.editTask(newName);
+  }
 }
-
-/*
-Search Button
-When you type something and click on +
-you will create a new task called "something"
-*/
-// I want the search button to be able to create a new task once you click on +
