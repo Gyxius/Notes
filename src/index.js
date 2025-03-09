95aeabdb7c1d1a251d6cc72dc975b854817f3979
Gyxius <mitsou.fortunat@gmail.com>
2025-03-09 22:56:14 +0100

    Add tasks to renderView

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { Tasks, TODO } from "./todo.js";
+import edit_icon from "./assets/icons/edit_icon.png";
+import delete_icon from "./assets/icons/delete_icon.png";
 
 class TodoController {
   constructor(todoModel, todoView) {
@@ -17,14 +19,29 @@ class TodoController {
     this.todo.editTask(taskId, newName);
   }
   updateView() {
-    this.view.renderConsole(this.todo.tasks);
+    this.view.renderTasksToConsole(this.todo.tasks);
+    this.view.renderTasksToBody(this.todo.tasks);
   }
 }
 
 class TodoView {
   removeTask(taskId) {}
-  renderConsole(tasks) {
+  renderTasksToConsole(tasks) {
     tasks.forEach((task) => console.log(task.name));
+  }
+  renderTasksToBody(tasks) {
+    const todoDiv = document.getElementById("todo");
+    tasks.forEach((task) => {
+      let taskDiv = document.createElement("div");
+      taskDiv.innerHTML = 
+      `
+        <div>
+            <input type="checkbox" id="${task.id}" name="${task.name}" checked />
+            <label for="${task.name}">${task.name}</label>
+        </div>
+      `;
+      todoDiv.appendChild(taskDiv);
+    });
   }
 }
 
@@ -40,9 +57,23 @@ class App {
   }
   createTasks() {
     this.toDoController.addTask("John");
+    this.toDoController.addTask("Bob");
     this.toDoController.updateView();
   }
 }
 
 const myApp = new App();
 myApp.createTasks();
+
+const todoDiv = document.getElementById("todo");
+
+// Add icons
+
+const editImg = document.createElement("img");
+const deleteImg = document.createElement("img");
+editImg.src = edit_icon;
+deleteImg.src = delete_icon;
+editImg.style.height = "20px";
+deleteImg.style.height = "20px";
+todoDiv.appendChild(editImg);
+todoDiv.appendChild(deleteImg);
